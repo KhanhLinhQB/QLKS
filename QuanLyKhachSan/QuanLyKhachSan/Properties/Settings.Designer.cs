9d4532ebfd1a00129d0007cf1361a500825423be
Nguyennhuhao99 <Nguyennhuhao999@gmail.com>
2021-09-19 10:14:34 +0700

    update hình ảnh

--- QuanLyKhachSan/QuanLyKhachSan/Properties/Settings.Designer.cs
+++ QuanLyKhachSan/QuanLyKhachSan/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace QuanLyKhachSan.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,8 @@ namespace QuanLyKhachSan.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=QLKSCN;Persist Security Info=True;User ID=sa;Passwo" +
-            "rd=sa")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-SQGBG1V.;Initial Catalog=QLKSCN;Persist Security Info=True;Us" +
+            "er ID=sa;Password=123456")]
         public string QLKSCConnectionString {
             get {
                 return ((string)(this["QLKSCConnectionString"]));
